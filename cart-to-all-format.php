afda4b6b11485d73fe2b0b1921d099e2018d4636
Genç Yazılımcı <46776442+gencyazilimcim@users.noreply.github.com>
2022-10-23 16:55:08 +0300

    Update cart-to-all-format.php

--- cart-to-all-format.php
+++ cart-to-all-format.php
@@ -9,24 +9,24 @@ function cart2csv()
     $csvList = array("Product Name", "SKU", "Price", "Quantity", "Subtotal"); // Excel header
 
     foreach (WC()->cart->get_cart() as $cart_item) {
-        $product            = $cart_item['data']; // Products Objects
+        $product         = $cart_item['data']; // Products Objects
 
-        $regular_price      = $product->get_regular_price(); // Normal Price
-        $sale_price         = $product->get_sale_price(); // Sale Price
-        $price              = $product->get_price(); // Last Price
-        $sku                = $product->get_sku(); // Stock Code
-        $name               = $product->get_name(); // Product Name
-        $quantity           = $cart_item['quantity']; // Cart Item Product Quantity
-        $line_subtotal      = $cart_item['line_subtotal'];  // Cart Item Subtotal
+        $regular_price   = $product->get_regular_price(); // Normal Price
+        $sale_price      = $product->get_sale_price(); // Sale Price
+        $price           = $product->get_price(); // Last Price
+        $sku             = $product->get_sku(); // Stock Code
+        $name            = $product->get_name(); // Product Name
+        $quantity        = $cart_item['quantity']; // Cart Item Product Quantity
+        $line_subtotal   = $cart_item['line_subtotal'];  // Cart Item Subtotal
       
         array_push($csvList, $name, $sku, $price, $quantity, $line_subtotal); // Add cart items to $csvList array
     }
 
     $arr = array_chunk($csvList, 5); // convert one-dimensional array to two-dimensional array php
     $jsArray = json_encode($arr); // convert array to json
-  
+    
     ?>
-    <script src="https://unpkg.com/export-from-json"></script>
+    <script src="https://unpkg.com/export-from-json@1.7.0/dist/umd/index.min.js"></script>
     <script>
     	var data = <?= $jsArray ?>;
 
@@ -39,8 +39,7 @@ function cart2csv()
         const downloadClick = document.getElementById("cartDownload");
         downloadClick.addEventListener("click", () => {
         	download("xml", "shopping-cart-download");
-        });
+        }, false);
     </script>
-    <?php
-  
+    <?php 
 }
